21164ff6f3334fcabbaf480a07a86e640c47dd1c
Masayuki Onishi <omccello@gmail.com>
2021-04-20 00:12:29 +0900

    Add Mock for select farmField

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,65 @@
-import React from "react";
-import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  FormControl,
+  Grid,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  Typography,
+} from "@material-ui/core";
 import MainContent from "./components/templates/MainContent";
 
+type FarmField = {
+  id: string;
+  name: string;
+};
+
 const App = (): JSX.Element => {
-  const onClick = () => {
-    console.log("Click!");
-    alert("アラート！");
+  const sampleFarmFields: FarmField[] = [
+    { id: "0", name: "未選択" },
+    { id: "1", name: "水田" },
+    { id: "2", name: "畑作" },
+    { id: "3", name: "牧草地" },
+  ];
+
+  const [farmField, setFarmField] = useState("0");
+  const handleChange = (e: React.ChangeEvent<{ value: string }>) => {
+    setFarmField(e.target.value);
   };
 
   return (
     <MainContent>
-      <Button color="primary" variant="outlined" onClick={() => onClick()}>
-        Hello World
-      </Button>
+      <Grid container>
+        <Grid mb={8} xs={12}>
+          <Typography variant="h4" style={{ fontWeight: "bold" }} gutterBottom>
+            ほ場データ入力
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            ほ場の種類と、土壌種類を入力する。
+          </Typography>
+        </Grid>
+        <Grid mb={4}>
+          <Typography variant="h5" color="secondary" gutterBottom>
+            ほ場の種類
+          </Typography>
+          <FormControl
+            variant="outlined"
+            style={{ width: "50%", marginBottom: "20px", minWidth: 120 }}
+          >
+            <Select
+              value={farmField}
+              onChange={handleChange}
+              input={<OutlinedInput name="farmField" />}
+            >
+              {sampleFarmFields.map((farmField, index) => (
+                <MenuItem key={index} value={farmField.id}>
+                  {farmField.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
     </MainContent>
   );
 };
