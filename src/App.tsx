a3b898cb7fb52369a97e5e03c4c01f08050a45eb
Masayuki Onishi <omccello@gmail.com>
2021-04-21 01:04:25 +0900

    soilType 選択のモック作成

--- src/App.tsx
+++ src/App.tsx
@@ -14,17 +14,32 @@ type FarmField = {
   name: string;
 };
 
+type SoilType = {
+  id: string;
+  name: string;
+};
+
 const App = (): JSX.Element => {
+  // TODO: FieldType に変更
   const sampleFarmFields: FarmField[] = [
-    { id: "0", name: "未選択" },
     { id: "1", name: "水田" },
     { id: "2", name: "畑作" },
     { id: "3", name: "牧草地" },
   ];
+  const sampleSoilTypes: SoilType[] = [
+    { id: "1", name: "露地畑" },
+    { id: "2", name: "施設畑" },
+    { id: "3", name: "果樹園" },
+  ];
 
-  const [farmField, setFarmField] = useState("0");
-  const handleChange = (e: React.ChangeEvent<{ value: string }>) => {
+  const [farmField, setFarmField] = useState("");
+  const [soilType, setSoilType] = useState("");
+
+  const handleChangeFarmField = (e: React.ChangeEvent<{ value: string }>) => {
     setFarmField(e.target.value);
+  };
+  const handleChangeSoilType = (e: React.ChangeEvent<{ value: string }>) => {
+    setSoilType(e.target.value);
   };
 
   return (
@@ -41,12 +56,26 @@ const App = (): JSX.Element => {
           <FormControl variant="outlined">
             <Select
               value={farmField}
-              onChange={handleChange}
+              onChange={handleChangeFarmField}
               input={<OutlinedInput name="farmField" />}
             >
               {sampleFarmFields.map((farmField, index) => (
                 <MenuItem key={index} value={farmField.id}>
                   {farmField.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <Typography variant="h3">土壌の種類</Typography>
+          <FormControl variant="outlined">
+            <Select
+              value={soilType}
+              onChange={handleChangeSoilType}
+              input={<OutlinedInput name="soilType" />}
+            >
+              {sampleSoilTypes.map((soilType, index) => (
+                <MenuItem key={index} value={soilType.id}>
+                  {soilType.name}
                 </MenuItem>
               ))}
             </Select>
