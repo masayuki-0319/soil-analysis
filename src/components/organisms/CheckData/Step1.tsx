09d7f81740096369028569a9d3904d86928a59d2
Masayuki Onishi <omccello@gmail.com>
2021-04-24 18:07:13 +0900

    ほ場と土壌selectboxを切り出し

---
+++ src/components/organisms/CheckData/Step1.tsx
@@ -0,0 +1,38 @@
+import React, { memo, VFC } from 'react';
+import { FormControl, Grid, Typography } from '@material-ui/core';
+import { useRecoilState } from 'recoil';
+
+import SelectSoilType from './SelectSoilType';
+import SelectFieldType from './SelectFieldType';
+import { analysisResultState } from '../../../store/analysisResultState';
+
+interface Props {}
+
+export const Step1: VFC<Props> = memo(() => {
+  const [analysisResult, setAnalysisResultState] = useRecoilState(analysisResultState);
+
+  const handleChange = (e: React.ChangeEvent<{ name: string; value: string }>) => {
+    setAnalysisResultState({ ...analysisResult, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <Grid container>
+      <Grid item mb={8} xs={12}>
+        <Typography variant="h2">ほ場データ入力</Typography>
+        <Typography variant="body1">ほ場の種類と、土壌種類を入力する。</Typography>
+      </Grid>
+      <Grid item mb={4} xs={12}>
+        <Typography variant="h3">ほ場の種類</Typography>
+        <FormControl variant="outlined">
+          <SelectFieldType defaultValue={analysisResult.fieldTypeId} onChange={handleChange} />
+        </FormControl>
+      </Grid>
+      <Grid item mb={4} xs={12}>
+        <Typography variant="h3">土壌の種類</Typography>
+        <FormControl variant="outlined">
+          <SelectSoilType defaultValue={analysisResult.soilTypeId} onChange={handleChange} />
+        </FormControl>
+      </Grid>
+    </Grid>
+  );
+});
--- src/components/pages/CheckData.tsx
+++ src/components/pages/CheckData.tsx
@@ -1,37 +1,13 @@
-import React, { memo, VFC } from 'react';
-import { FormControl, Grid, Typography } from '@material-ui/core';
-import SelectSoilType from '../organisms/CheckData/SelectSoilType';
-import SelectFieldType from '../organisms/CheckData/SelectFieldType';
-import { useRecoilState } from 'recoil';
-import { analysisResultState } from '../../store/analysisResultState';
+import { memo, VFC } from 'react';
+import { Box } from '@material-ui/core';
+import { Step1 } from '../organisms/CheckData/Step1';
 
 interface Props {}
 
 export const CheckData: VFC<Props> = memo(() => {
-  const [analysisResult, setAnalysisResultState] = useRecoilState(analysisResultState);
-
-  const handleChange = (e: React.ChangeEvent<{ name: string; value: string }>) => {
-    setAnalysisResultState({ ...analysisResult, [e.target.name]: e.target.value });
-  };
-
   return (
-    <Grid container>
-      <Grid item mb={8} xs={12}>
-        <Typography variant="h2">ほ場データ入力</Typography>
-        <Typography variant="body1">ほ場の種類と、土壌種類を入力する。</Typography>
-      </Grid>
-      <Grid item mb={4} xs={12}>
-        <Typography variant="h3">ほ場の種類</Typography>
-        <FormControl variant="outlined">
-          <SelectFieldType defaultValue={analysisResult.fieldTypeId} onChange={handleChange} />
-        </FormControl>
-      </Grid>
-      <Grid item mb={4} xs={12}>
-        <Typography variant="h3">土壌の種類</Typography>
-        <FormControl variant="outlined">
-          <SelectSoilType defaultValue={analysisResult.soilTypeId} onChange={handleChange} />
-        </FormControl>
-      </Grid>
-    </Grid>
+    <Box>
+      <Step1 />
+    </Box>
   );
 });
