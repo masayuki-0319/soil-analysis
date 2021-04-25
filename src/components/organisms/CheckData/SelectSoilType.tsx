364fb9f3d4f03b2527386ef478470185597ec7d5
Masayuki Onishi <omccello@gmail.com>
2021-04-26 00:55:18 +0900

    soilTypeのmasterData追加

--- src/components/organisms/CheckData/SelectSoilType.tsx
+++ src/components/organisms/CheckData/SelectSoilType.tsx
@@ -1,27 +1,17 @@
 import { memo, VFC } from 'react';
 import SelectBox from '../../atoms/selectbox/Selectbox';
 import { MenuItem } from '@material-ui/core';
+import { soilTypeData } from '../../../masterData/soilTypeData';
 
 interface Props {
   defaultValue: number;
   onChange: (props?: any) => void;
 }
 
-type SoilType = {
-  id: number;
-  name: string;
-};
-
 const SelectSoilType: VFC<Props> = memo((props) => {
   const { defaultValue, onChange } = props;
 
-  const sampleSoilTypes: SoilType[] = [
-    { id: 1, name: '露地畑' },
-    { id: 2, name: '施設畑' },
-    { id: 3, name: '果樹園' },
-  ];
-
-  const options = sampleSoilTypes.map((option, index) => (
+  const options = soilTypeData.map((option, index) => (
     <MenuItem key={index} value={option.id}>
       {option.name}
     </MenuItem>
---
+++ src/types/SoilType.ts
@@ -0,0 +1,4 @@
+export type SoilType = {
+  id: number
+  name: string
+}
