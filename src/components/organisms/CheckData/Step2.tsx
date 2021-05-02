f3f15318351c92aa2e7be37ef42781feec58fd94
Masayuki Onishi <omccello@gmail.com>
2021-05-02 23:45:55 +0900

    Step2 にスタイル追加

--- src/components/organisms/CheckData/Step2.tsx
+++ src/components/organisms/CheckData/Step2.tsx
@@ -1,9 +1,10 @@
 import React, { memo, VFC } from 'react';
-import { Grid, TextField, Typography } from '@material-ui/core';
+import { Grid, Paper, TextField } from '@material-ui/core';
 import { useRecoilState } from 'recoil';
 import { analysisResultState } from '../../../store/analysisResultState';
 import { analysisItems } from '../../../types/AnalysisResult';
 import SelectFormControl from '../../atoms/SelectFormControl';
+import { FormTopInfo } from '../../molecules/FormTopInfo';
 
 interface Props {}
 
@@ -16,7 +17,7 @@ export const Step2: VFC<Props> = memo(() => {
 
   const fields = analysisItems.map((item, index) => {
     return (
-      <Grid item mb={6} xs={6} key={index}>
+      <Grid item md={6} xs={12} style={{ marginBottom: 12 }} key={index}>
         <SelectFormControl>
           <TextField
             variant="outlined"
@@ -32,12 +33,11 @@ export const Step2: VFC<Props> = memo(() => {
   });
 
   return (
-    <Grid container>
-      <Grid item mb={8} xs={12}>
-        <Typography variant="h2">土壌分析結果入力</Typography>
-        <Typography variant="body1">※ 分析機器による土壌サンプルの分析結果を入力する。</Typography>
+    <Paper elevation={8} style={{ padding: 20, marginBottom: 21 }}>
+      <Grid container>
+        <FormTopInfo title="分析結果の入力" description="※ 分析機器による土壌サンプルの分析結果を入力する。" />
+        {fields}
       </Grid>
-      {fields}
-    </Grid>
+    </Paper>
   );
 });
