25283361a5ea648dc4b6ab0da768b29a67f5514d
Masayuki Onishi <omccello@gmail.com>
2021-05-05 18:23:29 +0900

    分析結果をタブ別に表示

--- src/components/pages/CheckData.tsx
+++ src/components/pages/CheckData.tsx
@@ -1,5 +1,5 @@
-import { memo, VFC } from 'react';
-import { Box } from '@material-ui/core';
+import React, { memo, useState, VFC } from 'react';
+import { Box, Tab, Tabs } from '@material-ui/core';
 import { Step1 } from '../organisms/CheckData/Step1';
 import { Step2 } from '../organisms/CheckData/Step2';
 import { Step3 } from '../organisms/CheckData/Step3';
@@ -8,12 +8,30 @@ import ReportChart from '../organisms/CheckData/ReportChart';
 interface Props {}
 
 export const CheckData: VFC<Props> = memo(() => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: any, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
     <Box>
       <Step1 />
       <Step2 />
-      <Step3 />
-      <ReportChart />
+      <Box sx={{ borderBottom: 4, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} centered>
+          <Tab label="土壌分析結果 ( テーブル )" />
+          <Tab label="土壌分析結果 ( チャート )" />
+        </Tabs>
+      </Box>
+      <Box>
+        <Box hidden={value !== 0}>
+          <Step3 />
+        </Box>
+        <Box hidden={value !== 1}>
+          <ReportChart />
+        </Box>
+      </Box>
     </Box>
   );
 });
